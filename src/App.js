9b40e99868b6550d59098a7338eb761aa13d460a
Blessiey <mogashoablessing676@gmail.com>
2024-08-12 15:22:09 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import ImproveSkills from "./components/InproveSkills";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Navbar/>
       <div className="container main">
        <HeroSection />
-        
+       <ImproveSkills /> 
 
       </div>
    
